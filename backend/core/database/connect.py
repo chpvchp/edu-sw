3adee302d273c371bfea4da302b3278defec2935
Cao Hoang Phuc <caohoangphuc025@gmail.com>
2026-06-19 21:36:34 +0700

    feat(db): add database connection module

---
+++ backend/core/database/connect.py
@@ -0,0 +1,17 @@
+import psycopg
+import os
+
+from dotenv import load_dotenv
+from psycopg.rows import dict_row
+
+load_dotenv("./.env")
+
+def connection_database():
+    return psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),  
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        row_factory=dict_row
+    )
