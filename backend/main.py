296433ee9fe55c2a6e4b727fce19dc07c9dfeb09
Cao Hoang Phuc <caohoangphuc025@gmail.com>
2026-06-20 11:45:59 +0700

    feat(api): add Endpoint API GET to get list exams

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,22 @@
 # source .venv/bin/activate
+# uvicorn main:app --reload
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from core.database.conndb import ConnDB
+
+db = ConnDB()
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/backend-api/bai-tap")
+def get_exams():
+    list_exams = db.get_list_exam()
+    return list_exams
