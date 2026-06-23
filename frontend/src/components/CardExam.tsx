14ca7fbb4c3744a9ee9b10deaabb31d25a081ea4
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-23 20:00:13 +0700

    feat(ui): replace hardcode to dynamic data - add dynamic data from api get - add Link

--- frontend/src/components/CardExam.tsx
+++ frontend/src/components/CardExam.tsx
@@ -1,28 +1,31 @@
-export default function CardExam() {
+import { Link } from "react-router-dom"
+import type { Exam } from "../type/exam.type"
+
+export default function CardExam({ id_exam, name_subject, name_exam, duration, created }: Exam) {
 
   const classNameInfo = "flex gap-4 justify-between text-gray-600"
 
   return (
-    <div className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
-      <h2 className="p-2 font-bold border border-gray-200 rounded-md text-center">Title Exam</h2>
+    <Link to={`/backend-api/bai-tap/${id_exam}`} className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
+      <h2 className="p-2 font-bold border border-gray-200 rounded-md text-center">{name_exam}</h2>
       <div>
 
         <div className={classNameInfo}>
-          <p>Subject:</p>
-          <p>Math</p>
+          <p>Môn:</p>
+          <p>{name_subject}</p>
         </div>
 
         <div className={classNameInfo}>
-          <p>Duration:</p>
-          <p>5 min</p>
+          <p>Thời gian làm bài:</p>
+          <p>{duration} phút</p>
         </div>
 
         <div className={classNameInfo}>
-          <p>Created:</p>
-          <p>2026-06-2 12h00</p>
+          <p>Ngày tạo:</p>
+          <p>{created}</p>
         </div>
 
       </div>
-    </div>
+    </Link>
   )
 }
