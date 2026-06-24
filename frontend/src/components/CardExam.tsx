798f23a3bd7c2b042632386b00d9cefa5d311446
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-24 11:27:09 +0700

    fix: rewrite link /bai-tap/{id_exam}

--- frontend/src/components/CardExam.tsx
+++ frontend/src/components/CardExam.tsx
@@ -6,7 +6,7 @@ export default function CardExam({ id_exam, name_subject, name_exam, duration, c
   const classNameInfo = "flex gap-4 justify-between text-gray-600"
 
   return (
-    <Link to={`/backend-api/bai-tap/${id_exam}`} className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
+    <Link to={`/bai-tap/${id_exam}`} className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
       <h2 className="p-2 font-bold border border-gray-200 rounded-md text-center">{name_exam}</h2>
       <div>
 
