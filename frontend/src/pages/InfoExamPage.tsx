2be54c533d15bcbe5492e7c23175e77282acde7c
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-24 12:28:58 +0700

    feat(ui): add InfoExamPage

---
+++ frontend/src/pages/InfoExamPage.tsx
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useInfoExam } from "../hook/useExam";
+import CardInfoExam from "../components/CardInfoExam";
+
+export default function InfoExamPage() {
+  const { id_exam } = useParams();
+  const idExam = Number(id_exam)
+  const { data } = useInfoExam( idExam );
+
+  return (
+    <main className="flex-1 flex flex-col bg-slate-50 items-center">
+      <CardInfoExam
+        data={data}
+      />
+      <div className="flex mt-2 p-2">
+        <p>--- Lịch sử làm bài ---</p>
+      </div>
+    </main>
+  )
+}
