b741896a1a916b47b1a5cf9373f1763caad4a291
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-26 12:40:00 +0700

    feat(convert): update script python convert md to json - refactor `is_answer` to `is_answer_four_choice` - add `is_path_image`, `is_correct` - refactor params `parse`, add name_exam, id_subject, duration - add logic new parse

--- backend/md_to_json.py
+++ backend/md_to_json.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import uuid
 
 def clean_line(line):
     return line.strip()
@@ -8,41 +9,78 @@ def clean_line(line):
 def is_question(line):
     return line.startswith("Câu")
 
-def is_answer(line):
+def is_answer_four_choice(line):
     return re.match(r"^[A-D]\.", line)
 
-def parse():
-    data = []
-    id_question = 0
-    id_answer = 0
+def is_path_image(line):
+    return line.startswith("![](images/")
+
+def is_correct(line):
+    if "ĐápÁnĐúng" not in line:
+        return False
+    else:
+        return True
+
+def parse(name_exam, id_subject, duration):
+    questions = []
+    exam = {
+        "id_exam": uuid.uuid4().hex,
+        "id_subject": id_subject,
+        "name_exam": name_exam,
+        "duration": duration,
+        "questions": questions
+    }
     
-    with open("./backend/a.md", "r", encoding="utf-8") as file:
+    with open(f"./.data/{name_exam}/exam.md", "r", encoding="utf-8") as file:
         for line in file:
             line = clean_line(line)
 
             if is_question(line):
+                id_question = uuid.uuid4().hex
                 
                 current_question = {
                     "id_question": id_question,
                     "question": line,
-                    "answers": []
+                    "type_question": "",
+                    "path_images": "",
+                    "answers": [],
+                    "results": {
+                        "explain": "",
+                        "correct_answer": ""
+                    }
                 }
                 
-                id_question += 1
-                data.append(current_question)
+                
+                questions.append(current_question)
+                
+            elif is_path_image(line) and current_question:
+                current_question["path_images"] = line[4:-1]
+                
             
-            elif is_answer(line) and current_question:
+            elif is_answer_four_choice(line) and current_question:
+                id_answer = uuid.uuid4().hex
+                
+                if current_question["type_question"] != "four_choice":
+                    current_question["type_question"] = "four_choice"
+                
+                if is_correct(line):
+                    current_question["results"]["correct_answer"] = id_answer
+                    line = line.replace("ĐápÁnĐúng", "")
+                    line = clean_line(line)
+                    
                 answer = {
                     "id_answer": id_answer,
                     "answer": line
                 }
-                id_answer += 1
                 current_question["answers"].append(answer)
-    return data
-
-
-data = {
-    "questions": parse()
-}
-data = json.dumps(data, indent=2, ensure_ascii=False)
-print(data)
+                
+    with open("./backend/json.json", "w", encoding="utf-8") as file:
+        data_json = json.dumps(exam, indent=2, ensure_ascii=False)
+        file.write(data_json)
+        
+        
+parse(
+    name_exam="Tính Đơn Điệu Và Cực Trị Của Hàm Số",
+    id_subject=1,
+    duration=30
+)
