659e17e91df32fa621a4178cba1a77e36d4785b9
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-28 16:08:20 +0700

    feat(component): add reusable CardQuestionFourChoice component

---
+++ frontend/src/components/CardQuestionFourChoice.tsx
@@ -0,0 +1,55 @@
+import { BASE_URL } from "../api/api";
+import RenderMarkDownLatex from "../components/RenderMarkDownLatex";
+import type { Question } from "../type/question.type";
+
+type CardQuestionFourChoiceProps = {
+  question: Question;
+}
+
+export default function CardQuestionFourChoice({ question }: CardQuestionFourChoiceProps) {
+  return (
+    <div className="p-2 flex flex-col border border-gray-400 rounded bg-white shadow">
+
+      <div className="p-2 flex flex-col border border-gray-400 rounded">
+
+        <h2 id={question?.id_question}>
+          <RenderMarkDownLatex
+            text={question?.question}
+          />
+        </h2>
+        
+        {question?.path_images && (
+          <img 
+            className="max-h-64 w-auto object-contain"
+            src={`${BASE_URL}/${question?.path_images}`} 
+          />
+        )}
+        
+      </div>
+
+      <div className="p-2 flex flex-col gap-2">
+
+        {question?.answers.map((answer) => (
+
+          <div className="flex gap-2">
+            <input 
+              type="radio"
+              name={question?.id_question}
+              value={answer?.id_answer}
+            />
+            <p>
+              <RenderMarkDownLatex 
+                text={answer?.answer}
+              />
+            </p>
+          </div>
+
+        ))}
+
+      </div>
+
+
+
+    </div>
+  )
+}
