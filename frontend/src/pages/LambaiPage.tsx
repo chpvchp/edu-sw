ac83b7767e96ff6aaa88541097273472f2c9e068
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-28 16:08:49 +0700

    feat(page): reusable component CardQuestionFourChoice and add SideBar

--- frontend/src/pages/LambaiPage.tsx
+++ frontend/src/pages/LambaiPage.tsx
@@ -1,57 +1,47 @@
-import { BASE_URL } from "../api/api";
 import { useParams } from "react-router-dom"
-import { useQuestions } from "../hook/useExam";
-import RenderMarkDownLatex from "../components/RenderMarkDownLatex";
+import { useInfoExam, useQuestions } from "../hook/useExam";
+import CardQuestionFourChoice from "../components/CardQuestionFourChoice";
 
 export default function LamBaiPage() {
   const { id_exam } = useParams()
   const idExam = String(id_exam)
   const { data } = useQuestions( idExam );
+  const { data: examInfo } = useInfoExam( idExam );
   
 
   return (
-    <main className="flex-1 flex flex-col bg-slate-50">
+    <main className="min-h-screen p-4 grid grid-cols-1 lg:grid-cols-2  items-start">
       
-      {data?.map((question) => (
+      <div className="px-8 py-4 flex flex-col gap-8">
+        {data?.map((question) => (
         
+          <CardQuestionFourChoice
+            key={question?.id_question}
+            question={question} 
+          />
+
+        ))}
+      </div>
+
+      <div className="px-8 py-4 sticky top-4">
+
         <div>
-
-          <div>
-
-            <h2>
-              <RenderMarkDownLatex
-                text={question?.question}
-              />
-            </h2>
-            
-            {question?.path_images && (
-              <img src={`${BASE_URL}/${question?.path_images}`} />
-            )}
-            
-          </div>
-
-          <div>
-
-            {question?.answers.map((answer) => (
-
-              <div>
-                <input type="radio" />
-                <p>
-                  <RenderMarkDownLatex 
-                    text={answer?.answer}
-                  />
-                </p>
-              </div>
-
-            ))}
-
-          </div>
-
-
-
+          <h1>{examInfo?.name_exam}</h1>
+        </div>
+        
+        <div className="p-2 border border-gray-400 rounded bg-white grid grid-cols-10 gap-2">
+          {data?.map((question) => (
+            <a
+              key={question.id_question}
+              href={`#${question.id_question}`}
+              className="flex items-center justify-center h-10 border border-gray-400 rounded transition duration-200 hover:bg-blue-600 hover:text-white hover:font-bold"
+            >
+              {question.order}
+            </a>
+          ))}
         </div>
 
-      ))}
+      </div>
 
     </main>
   )
