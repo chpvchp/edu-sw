62a58577ef3a000aa24be6da6a45ff7ae01cccd2
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-29 12:07:48 +0700

    style(ui): update ui and layout component in page - add max-w-7xl, mx-auto, grid-col-10 and 6:4 - add cardinfoexam basic

--- frontend/src/pages/LambaiPage.tsx
+++ frontend/src/pages/LambaiPage.tsx
@@ -15,9 +15,9 @@ export default function LamBaiPage() {
   }
   
   return (
-    <main className="min-h-screen p-4 grid grid-cols-1 lg:grid-cols-2  items-start">
+    <main className="min-h-screen max-w-7xl p-4 grid grid-cols-1 lg:grid-cols-10  items-start mx-auto">
       
-      <div className="px-8 py-4 flex flex-col gap-8">
+      <div className="px-8 py-4 flex flex-col lg:col-span-6 gap-8">
         {data?.map((question) => (
         
           <CardQuestionFourChoice
@@ -29,13 +29,18 @@ export default function LamBaiPage() {
         ))}
       </div>
 
-      <div className="px-8 py-4 sticky top-4">
+      <div className="px-8 py-4 sticky top-4 flex flex-col gap-4 lg:col-span-4">
 
-        <div>
-          <h1>{examInfo?.name_exam}</h1>
+        <div className="p-2 border border-gray-400 rounded flex flex-col">
+          <div className="p-2 border border-gray-400 rounded text-center font-bold">
+            <h1>{examInfo?.name_exam}</h1>
+          </div>
+          <div>
+
+          </div>
         </div>
         
-        <div className="p-2 border border-gray-400 rounded bg-white grid grid-cols-10 gap-2">
+        <div className="p-2 border border-gray-400 rounded bg-white grid grid-cols-10 gap-2 text-sm">
           {data?.map((question) => {
 
             const isAnswered = !!results[question.id_question];
@@ -44,7 +49,8 @@ export default function LamBaiPage() {
               <a
                 key={question.id_question}
                 href={`#${question.id_question}`}
-                className={`flex items-center justify-center h-10 border border-gray-400 rounded transition duration-200 hover:bg-blue-800 hover:text-white hover:font-bold ${isAnswered ? "bg-blue-600 font-bold text-white": "bg-white"}`}
+                className={`flex items-center justify-center border border-gray-400 rounded transition duration-200 hover:bg-blue-800 hover:text-white hover:font-bold 
+                  ${isAnswered ? "bg-blue-600 font-bold text-white": "bg-white"}`}
               >
                 {question.order}
               </a>
