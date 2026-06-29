aca8f64a995b552010518e59414de731beb3cd86
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-29 18:56:40 +0700

    feat(page): update LamBaiPage - add type `FourChoiceResult`, `TrueFalseResult` and `QuestionResult` - add onChangeInput object map with four_choice and true_false - add condition render component CardQuestion

--- frontend/src/pages/LambaiPage.tsx
+++ frontend/src/pages/LambaiPage.tsx
@@ -2,31 +2,98 @@ import { useParams } from "react-router-dom"
 import { useInfoExam, useQuestions } from "../hook/useExam";
 import CardQuestionFourChoice from "../components/CardQuestionFourChoice";
 import { useState } from "react";
+import CardQuestionTrueFalse from "../components/CardQuestionTrueFalse";
+
+type FourChoiceResult = {
+  type: "four_choice";
+  answer: string;
+};
+
+type TrueFalseResult = {
+  type: "true_false";
+  true_answer: string[];
+  false_answer: string[];
+};
+
+type QuestionResult = FourChoiceResult | TrueFalseResult;
 
 export default function LamBaiPage() {
   const { id_exam } = useParams()
   const idExam = String(id_exam)
   const { data } = useQuestions( idExam );
   const { data: examInfo } = useInfoExam( idExam );
-  const [ results, setResults ] = useState<Record<string, string>>({});
+  const [results, setResults] = useState<Record<string, QuestionResult>>({});
 
-  const onChangeInput = (idQuestion: string, idAnswer: string) => {
-    setResults((prev) => ({...prev, [idQuestion]: idAnswer}))
+  const onChangeInput = {
+    four_choice: (idQuestion: string, idAnswer: string) => {
+      setResults(prev => ({
+        ...prev,
+        [idQuestion]: {
+          type: "four_choice",
+          answer: idAnswer,
+        },
+      }));
+    },
+    true_false: (
+      idQuestion: string,
+      idAnswer: string,
+      value: boolean
+    ) => {
+      setResults(prev => {
+        const current =
+          prev[idQuestion]?.type === "true_false"
+            ? prev[idQuestion]
+            : {
+                type: "true_false" as const,
+                true_answer: [],
+                false_answer: [],
+              };
+
+        return {
+          ...prev,
+          [idQuestion]: {
+            type: "true_false",
+            true_answer: value
+              ? [...current.true_answer.filter(id => id !== idAnswer), idAnswer]
+              : current.true_answer.filter(id => id !== idAnswer),
+
+            false_answer: !value
+              ? [...current.false_answer.filter(id => id !== idAnswer), idAnswer]
+              : current.false_answer.filter(id => id !== idAnswer),
+          },
+        };
+      });
+    },
   }
-  
+
   return (
     <main className="min-h-screen max-w-7xl p-4 grid grid-cols-1 lg:grid-cols-10  items-start mx-auto">
       
       <div className="px-8 py-4 flex flex-col lg:col-span-6 gap-8">
-        {data?.map((question) => (
-        
-          <CardQuestionFourChoice
-            key={question?.id_question}
-            question={question}
-            onChange={onChangeInput}
-          />
+        {data?.map((question) => {
+          if (question.type_question === "four_choice") {
+            return (
+              <CardQuestionFourChoice
+                key={question.id_question}
+                question={question}
+                onChange={onChangeInput.four_choice} 
+              />
+            )
+          }
 
-        ))}
+          if (question.type_question === "true_false") {
+            return (
+              <CardQuestionTrueFalse
+                key={question.id_question}
+                question={question}
+                onChange={onChangeInput.true_false} 
+              />
+            )
+          }
+
+          return null;
+
+        })}
       </div>
 
       <div className="px-8 py-4 sticky top-4 flex flex-col gap-4 lg:col-span-4">
