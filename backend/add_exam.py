ea1ff0e35b748e6baf9e9e4c59ef8fe7acd262a1
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-06-29 11:45:44 +0700

    refactor: wrap script code into function with params

--- backend/add_exam.py
+++ backend/add_exam.py
@@ -4,21 +4,24 @@ from datetime import datetime
 
 db = ConnDB()
 
-with open("./backend/json/exam.json", "r", encoding="utf-8") as file_json:
-    exam = json.load(file_json)
-    
-    id_exam = exam["id_exam"]
-    id_subject = exam["id_subject"]
-    name_exam = exam["name_exam"]
-    duration = exam["duration"]
-    questions = exam["questions"]
-    created = datetime.now()
-    
-    db.add_exam(
-        id_exam=id_exam,
-        id_subject=id_subject,
-        name_exam=name_exam,
-        duration=duration,
-        questions=questions,
-        created=created
-    )
+def add_exam(name_exam):
+    with open(f"./backend/json/{name_exam}.json", "r", encoding="utf-8") as file_json:
+        exam = json.load(file_json)
+        
+        id_exam = exam["id_exam"]
+        id_subject = exam["id_subject"]
+        name_exam = exam["name_exam"]
+        duration = exam["duration"]
+        questions = exam["questions"]
+        created = datetime.now()
+        
+        db.add_exam(
+            id_exam=id_exam,
+            id_subject=id_subject,
+            name_exam=name_exam,
+            duration=duration,
+            questions=questions,
+            created=created
+        )
+
+add_exam()
