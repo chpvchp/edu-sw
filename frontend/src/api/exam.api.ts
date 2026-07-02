e5767d7ff7cd330d9727d61ff3f4895dc7422ee0
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-02 15:17:13 +0700

    feat(api): add postQuestionAnswer API to push student results to backend

--- frontend/src/api/exam.api.ts
+++ frontend/src/api/exam.api.ts
@@ -1,5 +1,6 @@
 import type { Exam } from "../type/exam.type";
 import type { InfoExam } from "../type/infoexam.type";
+import type { SubmitQuestionAnswer } from "../type/submit.type";
 import { api } from "./api"
 
 export const getExam = async (): Promise<Exam[]> => {
@@ -15,4 +16,9 @@ export const getInfoExam = async ( id_exam: string): Promise<InfoExam> => {
 export const getQuestionsExam = async ( id_exam: string) => {
   const { data } = await api.get(`/backend-api/bai-tap/${id_exam}/questions`);
   return data
+}
+
+export const postQuestionAnswer = async ( student_submit: SubmitQuestionAnswer ) => {
+  const { data } = await api.post(`/backend-api/submit`, student_submit)
+  return data;
 }
