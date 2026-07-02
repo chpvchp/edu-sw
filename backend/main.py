9a4e52b61e0270d9787cd75bc1fbaad03f75635c
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-02 15:21:49 +0700

    feat(api): add API Endpoint to submit student results and automatic scoring

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from core.database.conndb import ConnDB
+from core.type.type import SubmitQuestionAnswer
 
 db = ConnDB()
 
@@ -39,3 +40,46 @@ def get_questions_in_exam_(id_exam):
     if questions is None:
         raise HTTPException(status_code=404, detail="Not Found Questions in Exam")
     return questions
+
+@app.post("/backend-api/submit")
+def post_submit_exam(student_submit: SubmitQuestionAnswer):
+    correct_results = {}
+    questions = db.get_full_question_in_exam(id_exam=student_submit.id_exam).get("questions")
+    
+    num_questions = len(questions)
+    
+    for question in questions:
+        correct_results[question.get("id_question")] = {
+            "type": question.get("type_question"),
+            "results": question.get("results")
+        }
+            
+    score = 0
+    score_on_number = 10 / num_questions
+    
+    student_results = student_submit.results
+    
+    for id_question_student in student_results:
+        result = student_results[id_question_student]
+        if result.type == "four_choice":
+            if result.answer == correct_results[id_question_student]["results"]["correct_answer"]:
+                score += score_on_number
+        
+        elif result.type == "true_false":
+            score_on_true_false = score_on_number / 4
+            
+            for true_answer in result.true_answer:
+                if true_answer in correct_results[id_question_student]["results"]["true_answer"]:
+                    score += score_on_true_false
+                    
+            for false_answer in result.false_answer:
+                if false_answer in correct_results[id_question_student]["results"]["false_answer"]:
+                    score += score_on_true_false
+                
+        
+        elif result.type == "short_answer":
+            if result.answer == float(correct_results[id_question_student]["results"]["short_answer"]):
+                score += score_on_number
+                
+    print(score)
+    
