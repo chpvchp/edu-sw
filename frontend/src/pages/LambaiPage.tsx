3f9a3eaabe5829d622aaf8e7f36496c8e8102459
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-02 15:19:02 +0700

    feat: update LamBaiPage - add mutate and button to submit student result

--- frontend/src/pages/LambaiPage.tsx
+++ frontend/src/pages/LambaiPage.tsx
@@ -4,6 +4,7 @@ import CardQuestionFourChoice from "../components/CardQuestionFourChoice";
 import CardQuestionTrueFalse from "../components/CardQuestionTrueFalse";
 import CardQuestionShortAnswer from "../components/CardQuestionShortAnswer";
 import { useQuestionAnswer } from "../hook/useQuestionAnswer";
+import { useSubmitQuestionAnswer } from "../hook/useSubmit";
 
 export default function LamBaiPage() {
   const { id_exam } = useParams()
@@ -16,6 +17,7 @@ export default function LamBaiPage() {
     true_false,
     short_answer,
   } = useQuestionAnswer();
+  const { mutate } = useSubmitQuestionAnswer();
 
   return (
     <main className="min-h-screen max-w-7xl p-4 grid grid-cols-1 lg:grid-cols-10  items-start mx-auto">
@@ -59,16 +61,26 @@ export default function LamBaiPage() {
 
       <div className="px-8 py-4 sticky top-4 flex flex-col gap-4 lg:col-span-4">
 
-        <div className="p-2 border border-gray-400 rounded flex flex-col">
+        <div className="p-2 border border-gray-400 rounded flex flex-col gap-2">
           <div className="p-2 border border-gray-400 rounded text-center font-bold">
             <h1>{examInfo?.name_exam}</h1>
           </div>
-          <div>
-
+          <div className="flex justify-center items-center">
+            <button 
+              className="p-2 flex justify-center items-center bg-blue-600 text-white font-bold rounded border border-gray-50 transition duration-200 hover:bg-blue-800 hover:scale-105 active:scale-90"
+              onClick={() => {
+                mutate({
+                  id_exam: idExam,
+                  results: results,
+                })
+              }}
+            >
+              Nộp bài
+            </button>
           </div>
         </div>
         
-        <div className="p-2 border border-gray-400 rounded bg-white grid grid-cols-10 gap-2 text-sm">
+        <div className="p-2 border border-gray-400 rounded bg-white grid grid-cols-10 gap-2 text">
           {data?.map((question) => {
 
             const isAnswered = !!results[question.id_question];
