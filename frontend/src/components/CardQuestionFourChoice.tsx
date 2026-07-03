76ecb01b18b4605b4425faad2c99598936c8705e
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-03 12:27:18 +0700

    feat(component): add review question four choice

--- frontend/src/components/CardQuestionFourChoice.tsx
+++ frontend/src/components/CardQuestionFourChoice.tsx
@@ -5,10 +5,12 @@ import type { Question } from "../type/question.type";
 
 type CardQuestionFourChoiceProps = {
   question: Question;
+  data: boolean | any
   onChange: (idQuestion: string, idAnswer: string) => void;
+  results: boolean
 }
 
-function CardQuestionFourChoice({ question, onChange }: CardQuestionFourChoiceProps) {
+function CardQuestionFourChoice({ question, onChange, results, data }: CardQuestionFourChoiceProps) {
   return (
     <div className="p-2 flex flex-col border border-gray-400 rounded bg-white shadow">
 
@@ -31,23 +33,60 @@ function CardQuestionFourChoice({ question, onChange }: CardQuestionFourChoicePr
 
       <div className="p-2 flex flex-col gap-2">
 
-        {question?.answers.map((answer) => (
+        {question?.answers.map((answer) => {
 
-          <div className="flex gap-2" key={answer?.id_answer}>
-            <input 
-              type="radio"
-              name={question?.id_question}
-              value={answer?.id_answer}
-              onChange={() => onChange(question?.id_question, answer?.id_answer)}
-            />
-            <div>
-              <RenderMarkDownLatex 
-                text={answer?.answer}
-              />
-            </div>
-          </div>
+          if (!results) {
+            return (
+              <div className="flex gap-2" key={answer?.id_answer}>
+                <input 
+                  type="radio"
+                  name={question?.id_question}
+                  value={answer?.id_answer}
+                  onChange={() => onChange(question?.id_question, answer?.id_answer)}
+                />
+                <div>
+                  <RenderMarkDownLatex 
+                    text={answer?.answer}
+                  />
+                </div>
+              </div>
+            )
+          }
 
-        ))}
+          if (results) {
+
+            let classNameInput = ""
+
+            const isStudentAnswer = data?.student_results?.[question.id_question]?.answer === answer.id_answer;
+            const isCorrectAnswer = data?.correct_results?.[question.id_question]?.results.correct_answer === answer.id_answer;
+
+            if (isStudentAnswer) {
+              classNameInput = "accent-red-600"
+            } 
+
+            if (isCorrectAnswer) {
+              classNameInput = "accent-blue-600"
+            }
+
+            return (
+              <div className={`flex gap-2`} key={answer?.id_answer}>
+                <input
+                  className={classNameInput}
+                  type="radio"
+                  value={answer?.id_answer}
+                  checked={isStudentAnswer}
+                  readOnly
+                />
+                <div>
+                  <RenderMarkDownLatex 
+                    text={answer?.answer}
+                  />
+                </div>
+              </div>
+            )
+          }
+
+        })}
 
       </div>
 
