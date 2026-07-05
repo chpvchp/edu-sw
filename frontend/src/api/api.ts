ada8271f2ce37e55f67c7221572663103fe949b0
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-05 20:53:47 +0700

    feat(api): rechange BASE_URL (used proxy)

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://192.168.1.15:8000"
+// Use vite server proxy
+export const BASE_URL = ""
 
 export const api = axios.create({
   baseURL: BASE_URL,
