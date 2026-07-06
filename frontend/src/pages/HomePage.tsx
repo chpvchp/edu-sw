c176634f3e085119aa8919b14a0a02db100f15a9
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-06 20:12:29 +0700

    style: add note and replace style css

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
   return (
-    <main className="flex-1 flex">
-      This is HomePage
+    <main className="min-h-screen max-w-7xl flex-1 mx-auto">
+      <p className="pt-4">Trang đang phát triển ạ:3</p>
+      <p>Qua trang "Bài tập" để tìm bài ôn luyện nhá</p>
     </main>
   )
 }
