f7161b9de7df48a35a37ac44035767e803920055
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-06 20:12:12 +0700

    style: add padding in mainlayout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen font-sans flex flex-col">
+    <div className="min-h-screen font-sans flex flex-col p-2">
       <NavBar />
 
       <Outlet />
