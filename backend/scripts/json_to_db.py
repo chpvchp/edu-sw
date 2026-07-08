87e118ca69b3bd08aecd50d5ecc500f9f62a36eb
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-08 19:00:29 +0700

    refactor(scripts): rename add_exam to json_to_db

---
+++ backend/scripts/json_to_db.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# chmod +x add_exam.py
+
+from core.database.conndb import ConnDB
+import json
+from datetime import datetime
+from pathlib import Path
+
+db = ConnDB()
+
+def add_exam(name_exam):
+    ROOT = Path(__file__).resolve().parents[1]
+    path_exam = ROOT / "json" / f"{name_exam}.json"
+    with open(path_exam, "r", encoding="utf-8") as file_json:
+        exam = json.load(file_json)
+        
+        id_exam = exam["id_exam"]
+        id_subject = exam["id_subject"]
+        name_exam = exam["name_exam"]
+        duration = exam["duration"]
+        questions = exam["questions"]
+        created = datetime.now()
+        
+        db.add_exam(
+            id_exam=id_exam,
+            id_subject=id_subject,
+            name_exam=name_exam,
+            duration=duration,
+            questions=questions,
+            created=created
+        )
+
+add_exam(
+    name_exam="pdf"
+)
