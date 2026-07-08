e9e8b98f1459a788820a5cc1c5ef8050a885e507
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-07-08 19:44:13 +0700

    feat(scripts): add script convert img to pdf

---
+++ backend/scripts/img_to_pdf.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import img2pdf
+
+def img_to_pdf(name):
+    ROOT = Path(__file__).resolve().parents[1]
+    img_folder_path = ROOT / "imgs" / name
+    pdf_file = ROOT / "pdf" / f"{name}.pdf"
+    
+    images = sorted(img_folder_path.glob("*.jpg"))
+
+    with open(pdf_file, "wb") as file:
+        file.write(img2pdf.convert([img for img in images]))
+        
+img_to_pdf(
+    name="Tinh Bột và Cellulose (1)"
+)
