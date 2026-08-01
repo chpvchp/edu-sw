0e4bd7828384b736e330161860eaee4772ab5d78
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-08-01 09:52:43 +0700

    refactor: change web static

--- backend/scripts/img_to_md.py
+++
@@ -1,9 +0,0 @@
-from .img_to_pdf import img_to_pdf
-from .pdf_to_md import pdf_to_md
-
-def img_to_md(name):
-    img_to_pdf(name)
-    pdf_to_md(name)
-    
-name = input("Input Name: ")
-img_to_md(name)
