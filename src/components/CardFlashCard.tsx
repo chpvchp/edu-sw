24090688c56bfe455e4e2a3eb3c4b56f307b2172
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-09-03 21:29:35 +0700

    component: add component CardFlashCard.tsx and update FlashCardPage.tsx

---
+++ src/components/CardFlashCard.tsx
@@ -0,0 +1,44 @@
+import type { FlashCard } from "../type/flashcard.type"
+import { ConvertDate } from "../hooks/useConvert"
+
+
+export default function CardFlashCard ({ id_flashcard, name_flashcard, language, num_cards, updated, created, source } : FlashCard) {
+  const classNameInfo = "flex gap-4 justify-between text-gray-600"
+  console.log(id_flashcard, source)
+  return (
+    <section className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
+
+      <h2 className="p-2 font-bold border border-gray-200 rounded-md text-center">{name_flashcard}</h2>
+      <div>
+
+        <div className={classNameInfo}>
+          <p>Language:</p>
+          <p>{language}</p>
+        </div>
+
+        <div className={classNameInfo}>
+          <p>Count:</p>
+          <p>{num_cards}</p>
+        </div>
+
+        {/* <div className={classNameInfo}>
+          <p>Source:</p>
+          <p>{source}</p>
+        </div> */}
+
+        <div className={classNameInfo}>
+          <p>Updated:</p>
+          <p>{ConvertDate(updated)}</p>
+        </div>
+
+        <div className={classNameInfo}>
+          <p>Created:</p>
+          <p>{ConvertDate(created)}</p>
+        </div>
+
+        
+
+      </div>
+    </section>
+  )
+}
--- src/pages/FlashCardPage.tsx
+++ src/pages/FlashCardPage.tsx
@@ -1,21 +1,56 @@
-import FlashCard from "../components/FlashCard"
+// import FlashCard from "../components/FlashCard"
+
+import { useEffect } from "react";
+import { useListFlashCard } from "../hooks/useFlashCard"
+import CardFlashCard from "../components/CardFlashCard";
+import type { FlashCard } from "../type/flashcard.type";
 
 export default function FlashCardPage() {
 
-  const data = {
-    vocab: "Artificial Intelligence", mean: "Trí tuệ nhân tạo", pos: "noun phrase", ipa: "/ˌɑːrtɪˈfɪʃəl ɪnˈtelɪdʒəns/", example: "Artificial intelligence is changing the world"
-  }
+  const { data, isLoading, error } = useListFlashCard();
+
+
+  useEffect(() => {
+    document.title = "Flashcard | Edu SW"
+  }, [])
+
+  if (isLoading) return <p className="p-4 mx-auto">Đang lấy danh sách bài tập...</p>;
+  if (error) return <p className="p-4 mx-auto">Máy chủ lỗi!</p>;
+
+
+  console.log(data)
 
   return (
-    <main className="min-h-screen flex-1 flex flex-col justify-center items-center">
-      
-      <FlashCard 
+    <main className="min-h-screen max-w-7xl flex-1 mx-auto">
+      <div className="flex justify-center">
+        <p className="p-4 italic">Chọn các flashcard bên dưới để rèn luyện nhé :3</p>
+      </div>
+
+      <div className="p-2 flex flex-col lg:grid lg:grid-cols-4 justify-center gap-2 lg:gap-6 lg:mx-auto">
+
+        {data?.map((card: FlashCard) => (
+          <CardFlashCard
+            key={card?.id_flashcard}
+            id_flashcard={card?.id_flashcard}
+            name_flashcard={card?.name_flashcard}
+            language={card?.language}
+            num_cards={card?.num_cards}
+            updated={card?.updated}
+            created={card?.created}
+            source={card?.source}
+          />
+        ))}
+        
+
+      </div>
+
+      {/* <FlashCard 
         vocab={data?.vocab}
         pos={data?.pos}
         ipa={data?.ipa}
         mean={data?.mean}
         example={data?.example}
-      />
+      /> */}
 
     </main>
   )
