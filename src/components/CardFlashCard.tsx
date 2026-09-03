16d78a7432d57fd5fa475e98f813df7d7d6ba091
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-09-03 23:02:12 +0700

    refactor: change to Link

--- src/components/CardFlashCard.tsx
+++ src/components/CardFlashCard.tsx
@@ -1,12 +1,13 @@
 import type { FlashCard } from "../type/flashcard.type"
 import { ConvertDate } from "../hooks/useConvert"
+import { Link } from "react-router-dom"
 
 
 export default function CardFlashCard ({ id_flashcard, name_flashcard, language, num_cards, updated, created, source } : FlashCard) {
   const classNameInfo = "flex gap-4 justify-between text-gray-600"
   console.log(id_flashcard, source)
   return (
-    <section className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
+    <Link to={`/flashcard/${id_flashcard}/practice`} className="p-2 flex flex-col gap-2 border border-gray-200 rounded-md bg-white shadow transition duration-300 hover:scale-110 hover:shadow-xl">
 
       <h2 className="p-2 font-bold border border-gray-200 rounded-md text-center">{name_flashcard}</h2>
       <div>
@@ -39,6 +40,6 @@ export default function CardFlashCard ({ id_flashcard, name_flashcard, language,
         
 
       </div>
-    </section>
+    </Link>
   )
 }
