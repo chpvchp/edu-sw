10aacf1f499620068c24e07ef2f7f37849a4c224
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-09-03 23:00:54 +0700

    feat(api): update flashcard.api.ts
    - add flashCardCache, loadFlashcarddata and getCard

--- src/api/flashcard.api.ts
+++ src/api/flashcard.api.ts
@@ -1,6 +1,29 @@
-import type { FlashCard } from "../type/flashcard.type";
+import type { Cards, FlashCard } from "../type/flashcard.type";
 import { fetchJson } from "./api";
 
 const FLASHCARDS_INDEX_PATH = "/data/flashcards/index.json";
+const flashCardCache = new Map<string, FlashCardData>();
+
+type FlashCardData = FlashCard & {
+  cards: Cards[]
+}
+
+async function loadFlashcarddata(idFlashcard: string): Promise<FlashCardData> {
+  const cachedFlashcard = flashCardCache.get(idFlashcard);
+
+  if (cachedFlashcard) {
+    return cachedFlashcard;
+  }
+
+  const flashcard = await fetchJson<FlashCardData>(`/data/flashcards/${idFlashcard}.json`);
+  flashCardCache.set(idFlashcard, flashcard);
+  return flashcard;
+}
+
 
 export const getFlashCards = async (): Promise<FlashCard[]> => fetchJson<FlashCard[]>(FLASHCARDS_INDEX_PATH);
+
+export const getCard = async (idFlashcard: string): Promise<Cards[]> => {
+  const flashcard = await loadFlashcarddata(idFlashcard);
+  return flashcard.cards
+}
