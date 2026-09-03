1f6bb46f78db94b4043ac75099879ba45b4052b5
Cao Hoàng Phúc <caohoangphuc025@gmail.com>
2026-09-03 23:03:26 +0700

    feat(hook): add useCards and change type

--- src/hooks/useFlashCard.ts
+++ src/hooks/useFlashCard.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import type { FlashCard } from "../type/flashcard.type";
-import { getFlashCards } from "../api/flashcard.api";
+import type { Cards, FlashCard } from "../type/flashcard.type";
+import { getCard, getFlashCards } from "../api/flashcard.api";
 
 export const useListFlashCard = () => {
   const [data, setData] = useState<FlashCard[] | undefined>();
@@ -37,3 +37,39 @@ export const useListFlashCard = () => {
 
   return { data, isLoading, error };
 };
+
+export const useCards = ( id_flashcard: string ) => {
+  const [data, setData] = useState<Cards[]>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    setIsLoading(true);
+    setIsError(false);
+
+    getCard(id_flashcard)
+      .then((questions) => {
+        if (isMounted) {
+          setData(questions);
+        }
+      })
+      .catch(() => {
+        if (isMounted) {
+          setIsError(true);
+        }
+      })
+      .finally(() => {
+        if (isMounted) {
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id_flashcard]);
+
+  return { data, isLoading, isError };
+}
