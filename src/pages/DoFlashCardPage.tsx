16030d300228242d4cab92029c49e2705efa8460
CaoPhuc <caohoangphuc025@gmail.com>
2026-09-05 21:54:09 +0700

    feat(ui): add card info flashcard

--- src/pages/DoFlashCardPage.tsx
+++ src/pages/DoFlashCardPage.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom"
-import { useCards } from "../hooks/useFlashCard";
+import { useCards, useInfoFlashcard } from "../hooks/useFlashCard";
 import FlashCard from "../components/FlashCard";
 import { useState } from "react";
 import { ArrowRight, ArrowLeft } from 'lucide-react';
+import CardFlashCard from "../components/CardFlashCard";
 
 export default function DoFlashCardPage () {
 
@@ -10,11 +11,21 @@ export default function DoFlashCardPage () {
   const idFlashcard = String(id_flashcard)
   const { data, isLoading, isError } = useCards(idFlashcard);
   const [ order, setOrder] = useState(0);
-  const disableButtonBack = order === 0
-  const disableButtonContinue = order === 1
+  const { data: flashcardInfo} = useInfoFlashcard(idFlashcard)
 
-  console.log(data)
-  console.log("order: ", order)
+  console.log(flashcardInfo)
+
+  if (!flashcardInfo) {
+    return (
+      <p className="p-4 mx-auto">Không thấy thông tin</p>
+    )
+  }
+
+  const disableButtonBack = order === 0
+  const disableButtonContinue = order === flashcardInfo.num_cards - 1
+
+  // console.log(data)
+  // console.log("order: ", order)
 
   if (isLoading) return <p className="p-4 mx-auto">Đang tải đề...</p>
   if (isError) return <p className="p-4 mx-auto">Máy chủ lỗi!</p>
@@ -34,10 +45,20 @@ export default function DoFlashCardPage () {
 
   return (
     <main className="min-h-screen flex-1">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-8">
 
-        <div className="p-5">
 
+        <div className="mt-8 mx-auto">
+          <CardFlashCard
+            key={flashcardInfo?.id_flashcard}
+            id_flashcard={flashcardInfo?.id_flashcard}
+            name_flashcard={flashcardInfo?.name_flashcard}
+            language={flashcardInfo?.language}
+            num_cards={flashcardInfo?.num_cards}
+            updated={flashcardInfo?.updated}
+            created={flashcardInfo?.created}
+            source={flashcardInfo?.source}
+          />
         </div>
 
         <div className="flex flex-col gap-6 items-center justify-center">
